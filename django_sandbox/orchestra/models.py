bc4f552127cd2ac99c871ec6268e588ee1d6ae27
Tyson Price <tysonprice@Tysons-MacBook-Pro.local>
2021-08-14 16:11:35 -0700

    fixed incorect reference to variable

--- django_sandbox/orchestra/models.py
+++ django_sandbox/orchestra/models.py
@@ -6,8 +6,7 @@ from django.db import models
 class Instrument(models.Model):
     name = models.CharField(max_length = 100)
     def __str__(self):
-        return name
-
+        return self.name
 
 
 class Musician(models.Model):
@@ -15,5 +14,5 @@ class Musician(models.Model):
     can_play = models.ManyToManyField(Instrument)
 
     def __str__(self):
-        return name
+        return self.name
 
