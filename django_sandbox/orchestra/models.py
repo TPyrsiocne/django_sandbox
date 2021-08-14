afb4b21c1ed70e020314b7ab6cbbfc21c693d509
Tyson Price <tysonprice@Tysons-MacBook-Pro.local>
2021-08-14 15:35:44 -0700

    made Instrument and Musician models

--- django_sandbox/orchestra/models.py
+++ django_sandbox/orchestra/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Instrument(models.Model):
+    name = models.CharField(max_length = 100)
+    def __str__(self):
+        return name
+
+
+
+class Musician(models.Model):
+    name = models.CharField(max_length = 100)
+    can_play = models.ManyToManyField(Instrument)
+
+    def __str__(self):
+        return name
+
