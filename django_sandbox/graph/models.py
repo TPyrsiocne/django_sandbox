e65186b5b76a643f81e4f36a633098ee9b6dc6a3
Tyson Price <tysonprice@Tysons-MacBook-Pro.local>
2021-08-15 10:03:25 -0700

    added __str__  method to Edge

--- django_sandbox/graph/models.py
+++ django_sandbox/graph/models.py
@@ -15,5 +15,8 @@ class Edge(models.Model):
     to_node = models.ForeignKey(Node, related_name = 'to_node',on_delete=models.CASCADE)
     label = models.CharField(max_length = 100)
 
+    def __str__(self):
+        return self.from_node.label + " -> " + self.to_node.label
+
 
 
