380373120bd0e71f36f319dfb4c0d5fc3250197e
Tyson Price <tysonprice@Tysons-MacBook-Pro.local>
2021-08-15 09:56:16 -0700

    implemented model Node with nonsymetrical ManyToMany relationship to itsself through Edge with edge also having a field for a label (as well as the from and to fields)

--- django_sandbox/graph/models.py
+++ django_sandbox/graph/models.py
@@ -2,15 +2,18 @@ from django.db import models
 
 
 class Node(models.Model):
-    """points_to = models.ManyToManyField("self", symmetrical=False)"""
+    points_to = models.ManyToManyField("self",symmetrical=False,through='Edge')
     label = models.CharField(max_length=10)
 
     def __str__(self):
         return self.label
 
 
-"""class Edge(models.Model):
+class Edge(models.Model):
+    #not sure what 'on_delete = models.CASCADE' does
+    from_node = models.ForeignKey(Node, related_name = 'from_node',on_delete=models.CASCADE)
+    to_node = models.ForeignKey(Node, related_name = 'to_node',on_delete=models.CASCADE)
     label = models.CharField(max_length = 100)
-"""
+
 
 
