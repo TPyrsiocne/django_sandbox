7e5ef385e38f143ca93b571d73d8cd8202838698
Tyson Price <tysonprice@Tysons-MacBook-Pro.local>
2021-08-19 13:59:32 -0700

    made simple view for /orchestra/ with out templet yet

--- django_sandbox/django_sandbox/urls.py
+++ django_sandbox/django_sandbox/urls.py
@@ -14,5 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+urlpatterns = [
+    path('orchestra/', include('orchestra.urls')),
+    path('admin/', admin.site.urls),
+]
 
