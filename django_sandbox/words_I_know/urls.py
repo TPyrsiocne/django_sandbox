43d089f2ccba43c59fac2fbc69617ba7a61514c8
Tyson Price <tysonprice@Tysons-MacBook-Pro.local>
2021-08-19 18:13:44 -0700

    words_I_know app now has ability for visiters to make new accoutns, log in, and log out. Also, an error page is displaed if account creation is unsuccessfull

--- django_sandbox/words_I_know/urls.py
+++ django_sandbox/words_I_know/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("register/", views.register, name = "register")
 ]
--- django_sandbox/words_I_know/views.py
+++ django_sandbox/words_I_know/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.contrib.auth import login
+from django.shortcuts import redirect, render, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
 
 def dashboard(request):
     return render(request, "words_I_know/dashboard.html")
+
+def register(request):
+    if request.method == "GET":
+        return render(request, 'words_I_know/register.html', {'form' : UserCreationForm})
+    elif request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse('dashboard'))
+        else:
+            return render(request, 'words_I_know/invalid_username.html')
